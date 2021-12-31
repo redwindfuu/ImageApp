7f954a40b9b24d7e15b6f76ab5694fc2f600b2fa
Mã Chấn Phong <75919476+redwindfuu@users.noreply.github.com>
2021-12-31 13:07:52 +0700

    user model

--- src/app/site/siteController.js
+++ src/app/site/siteController.js
@@ -1,11 +1,31 @@
 const siteservice = require('./siteService');
 const db = require('../../config/firebase')
-
+const userModel = require('../../model/User')
 class SiteController {
     // [GET]: /
     async index(req, res, next) {
-        const data = await db.collection('USER').doc('NnY9dphakpjm0qIXkeHH').get()
-        res.send(data.data())
+        const data = await db.collection('USER').get()
+        var h = []
+        data.forEach((doc) => {
+            h.push(new userModel(doc.id,doc.data()));
+        //    console.log(doc.id, '=>', doc.data());
+          })
+      
+        var dat = {
+            name :'lệ quyên'
+        }
+        var n = await db.collection('USER').where("name", "==", " lệ quyên 1").get() ;
+       // await n.save()
+        if (n.empty) {
+        console.log('No matching documents.');
+        } 
+        console.log(n) 
+        var p
+        n.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+        p = new userModel(doc.id, doc.data())
+        });
+        res.send(p)
     }
 
 }
--- src/model/User.js
+++ src/model/User.js
@@ -1,13 +1,51 @@
-exports.User = (user)=>{
-    this.id = '' || user.id;
-    this.password = '' || user.password;
-    this.publickey = '' || user.publickey;
-    this.privatekey = '' || user.privatekey;
-    this.show = function(){
-        return {
-            id: this.id,
-            password: this.password,
-            publickey: this.publickey,
-        }
+const db = require("../config/firebase");
+class User {
+  constructor(id, data) {
+    this.id = id || "";
+    this.data = data || {};
+  }
+  async save() {
+    if (!this.id) {
+      var check = await db
+        .collection("USER")
+        .where("user", "==", this.data.user)
+        .get();
+      if (check.empty) {
+        const obj = await db.collection("USER").add(this.data);
+        return obj;
+      }
+      return 0;
+    } else {
+      const obj = await db.collection("USER").doc(this.id);
+      const res = await obj.update(this.data);
+      return res;
     }
+    return 0;
+  }
+  set(data){
+    this.data = data
+  }
+  getPrivate(){
+      return {
+          pri : this.data.pri,
+          pass : this.data.pass,
+          n : this.data.n,
+      }
+  }
+  getValues(){
+    return {
+        name: this.data.name,
+        pub : this.data.pub,
+        n : this.data.n,
+    }
+  }
+  printInfo() {
+    if (this.id) {
+      console.log(this.id, "has value :", this.data);
+    } else {
+      console.log(" no id and has value :", this.data);
+    }
+  }
 }
+
+module.exports = User;
